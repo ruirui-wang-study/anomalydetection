af0b3ad9b627b643926a4ffbb45ddf32b2fa86ab
Jasmine0722 <962302845@qq.com>
2023-07-26 23:02:14 +0800

    add files

---
+++ preprocess.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 16:31:20 2019
+
+@author: ELİF NUR
+"""
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+
+
+def loadData(fromPath, LabelColumnName, labelCount
+             ):  #This method to read the csv file and change the label feature
+
+    data_ = pd.read_csv(fromPath,sep=',')
+
+    if labelCount == 2:
+        dataset = data_
+        dataset[LabelColumnName] = dataset[LabelColumnName].apply({
+            'DoS':
+            'Anormal',
+            'BENIGN':
+            'Normal',
+            'DDoS':
+            'Anormal',
+            'PortScan':
+            'Anormal'
+        }.get)
+    else:
+        dataset = data_
+    # data_.set_index(' Flow Duration').plot()
+    # data = dataset[LabelColumnName].value_counts()
+    # data.plot(kind='pie')
+    # duration = dataset[' Flow Duration']
+    # duration.plot()
+
+    # featureList = dataset.loc[:,[' Flow Duration','Flow Bytes/s',' Flow Packets/s',' Flow IAT Mean',' Fwd IAT Mean','Fwd Packets/s',' Bwd Packets/s',' Packet Length Mean','Init_Win_bytes_forward',' Init_Win_bytes_backward']]
+    featureList=dataset.drop([LabelColumnName], axis=1)
+    return dataset, featureList
+
+
+def datasetSplit(
+        df,
+        LabelColumnName):  #This method is to separate the dataset as X and y.
+    labelencoder = LabelEncoder()
+    df.iloc[:, -1] = labelencoder.fit_transform(df.iloc[:, -1])
+    # X = df.loc[:,[' Flow Duration','Flow Bytes/s',' Flow Packets/s',' Flow IAT Mean',' Fwd IAT Mean','Fwd Packets/s',' Bwd Packets/s',' Packet Length Mean','Init_Win_bytes_forward',' Init_Win_bytes_backward']]
+
+    X = df.drop([LabelColumnName], axis=1)
+    X = np.array(X)
+    X = X.T
+    for column in X:  #Control of values in X
+        median = np.nanmedian(column)
+        column[np.isnan(column)] = median
+        column[column == np.inf] = 0
+        column[column == -np.inf] = 0
+    X = X.T
+    scaler = preprocessing.MinMaxScaler()
+    X = scaler.fit_transform(X)
+    y = df[[LabelColumnName]]
+    return X, y
+
+
+def train_test_dataset(
+    df
+):  #This method is to separate the dataset as X_train,X_test,y_train and y_test.
+    labelencoder = LabelEncoder()
+    df.iloc[:, -1] = labelencoder.fit_transform(df.iloc[:, -1])
+    # X = df.loc[:,[' Flow Duration','Flow Bytes/s',' Flow Packets/s',' Flow IAT Mean',' Fwd IAT Mean','Fwd Packets/s',' Bwd Packets/s',' Packet Length Mean','Init_Win_bytes_forward',' Init_Win_bytes_backward']]
+
+    X = df.drop([' Label'], axis=1)
+    y = df[[' Label']]
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        train_size=0.7,
+                                                        test_size=0.3,
+                                                        random_state=0,
+                                                        stratify=y)
+    X_train = np.array(X_train)
+    X_train = X_train.T
+    for column in X_train:
+        median = np.nanmedian(column)
+        column[np.isnan(column)] = median
+        column[column == np.inf] = 0
+        column[column == -np.inf] = 0
+    X_train = X_train.T
+    y_train = np.array(y_train)
+    y_train = y_train.T
+    for column in y_train:
+        median = np.nanmedian(column)
+        column[np.isnan(column)] = median
+        column[column == np.inf] = 0
+        column[column == -np.inf] = 0
+    y_train = y_train.T
+    X_test = np.array(X_test)
+    X_test = X_test.T
+    for column in X_test:
+        median = np.nanmedian(column)
+        column[np.isnan(column)] = median
+        column[column == np.inf] = 0
+        column[column == -np.inf] = 0
+    X_test = X_test.T
+    y_test = np.array(y_test)
+    y_test = y_test.T
+    for column in y_test:
+        median = np.nanmedian(column)
+        column[np.isnan(column)] = median
+        column[column == np.inf] = 0
+        column[column == -np.inf] = 0
+    y_test = y_test.T
+
+    return X_train, X_test, y_train, y_test
---
+++ qlearning.py
@@ -0,0 +1,124 @@
+# 使用Q-Learning算法和决策树分类模型对异常流量进行检测，模型如下：
+
+# 状态空间：每个状态可以表示为一个向量，其中包含一组特征。
+# 动作空间：动作空间表示特征选择的操作。例如，每个动作可以表示选择或丢弃某个特征。
+# 奖励函数：奖励函数评估每个动作的质量。可以根据特定的目标来定义奖励函数，例如最大化分类准确率、最小化误报率或最大化检测率等。
+# Q-learning算法：使用Q-learning算法或其他适合的强化学习算法进行特征选择。在每个时间步骤中，代理根据当前状态选择一个动作，执行该动作并观察奖励和下一个状态。
+# 然后，使用Q-learning算法更新Q值函数，以指导代理在未来选择更好的动作。
+# 迭代训练：代理在状态空间中不断迭代地选择动作、更新Q值函数，以逐渐优化特征选择策略。可以通过设置迭代次数或其他终止条件来控制训练的停止。
+# 特征选择与模型构建：在训练完成后，根据学习到的策略选择最相关的特征子集。然后，使用这些选择的特征来构建模型，如分类器、聚类器或异常检测器等。
+
+
+对已有模型进行自适应性变化的方法
+已有sdn流表下发的方式
+
+动态网络环境（找出关键的特征）怎样影响异常流量的检测
+流量的变化与异常检测
+动态：拓扑、节点、拥塞 无关
+攻击类型的动态性-》机器学习算法的选择
+各种方法在异常检测中的研究现状
+流量变化的典型场景（物联网等
+机器学习依赖于已有数据集（问题点）--》强化学习
+机器学习模型在线优化方法
+sdn中对异常流量的检测方法
+sdn怎么动态改变数据平面的转发（下发转发表 已有
+控制平面用哪个模型效果好（作对比）
+控制平面里模型的更新能力和能不能转化为转发表
+数据平面    轻量级最优特征提取模型
+实现模型的限制
+qlearning在数据平面的实现
+特征怎么传到控制平面
+是否适用
+
+
+
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import preprocess
+
+fromPath = "dataset.csv"
+LabelColumnName = ' Label'
+dataset, featureList = preprocess.loadData(fromPath, LabelColumnName, 2)
+X, y = preprocess.datasetSplit(dataset, LabelColumnName)
+X_train, X_test, y_train, y_test = preprocess.train_test_dataset(dataset)
+
+
+
+
+# Q-learning算法
+class QLearning:
+    def __init__(self, state_space, action_space, learning_rate=0.1, discount_factor=0.9):
+        self.state_space = state_space
+        self.action_space = action_space
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.q_table = np.zeros((len(state_space), len(action_space)))
+
+    def choose_action(self, state):
+        state_index = self.state_space.index(state)
+        q_values = self.q_table[state_index, :]
+        max_q_value = np.max(q_values)
+        max_action_indices = np.where(q_values == max_q_value)[0]
+        chosen_action_index = np.random.choice(max_action_indices)
+        chosen_action = self.action_space[chosen_action_index]
+        return chosen_action
+
+    def update_q_table(self, state, action, next_state, reward):
+        state_index = self.state_space.index(state)
+        next_state_index = self.state_space.index(next_state)
+        action_index = self.action_space.index(action)
+        max_q_value = np.max(self.q_table[next_state_index, :])
+        current_q_value = self.q_table[state_index, action_index]
+        new_q_value = (1 - self.learning_rate) * current_q_value + self.learning_rate * (reward + self.discount_factor * max_q_value)
+        self.q_table[state_index, action_index] = new_q_value
+
+# 定义决策树特征选择函数
+def select_features(X, y, feature_combination):
+    selected_X = X[:, feature_combination]
+    # 创建决策树分类器
+    clf = DecisionTreeClassifier()
+    clf.fit(selected_X, y)
+    return clf
+
+# 定义异常检测函数
+def detect_anomalies(q_learning, X, y):
+    num_features = X.shape[1]
+    selected_features = []
+    for i in range(num_features):
+        # 使用Q-learning选择最佳特征
+        state = tuple(selected_features)
+        action = q_learning.choose_action(state)
+        selected_features.append(action)
+
+        # 训练决策树模型并进行预测
+        clf = select_features(X, y, selected_features)
+        predictions = clf.predict(X[:, selected_features])
+
+        # 计算奖励
+        accuracy = np.mean(predictions == y)
+        reward = 2 * accuracy - 1
+
+        # 更新Q值
+        next_state = tuple(selected_features)
+        q_learning.update_q_table(state, action, next_state, reward)
+
+    return selected_features
+
+# 示例数据
+# X = np.array([[1, 2, 3, 4],
+#               [2, 3, 4, 5],
+#               [3, 4, 5, 6],
+#               [4, 5, 6, 7]])
+# y = np.array([0, 0, 1, 1])
+
+# 定义状态空间和动作空间
+state_space = [tuple([])]
+action_space = list(range(X.shape[1]))
+
+# 创建Q-learning对象并进行训练
+q_learning = QLearning(state_space, action_space)
+selected_features = detect_anomalies(q_learning, X, y)
+
+print("Selected Features:", selected_features)
